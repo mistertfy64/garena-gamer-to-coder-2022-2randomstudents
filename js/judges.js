29817ff2fe8596d6510b1b800783afa6a0e4f209
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2022-10-29 22:54:37 +0700

    feature: added judges page

---
+++ js/judges.js
@@ -0,0 +1,48 @@
+const JUDGE_INFORMATION = {};
+
+function initialize() {
+	getAndRenderData();
+}
+
+async function getAndRenderData() {
+	fetch("https://gamertocoder.garena.co.th/api/assets")
+		.then((response) => {
+			if (response.status === 200) {
+				return response.json();
+			}
+			// handle error
+		})
+		.then((data) => {
+			for (let i = 0; i < data.characters.length; i++) {
+				JUDGE_INFORMATION[i] = {
+					name: data.characters[i]
+						.replace(
+							"https://cdngarenanow-a.akamaihd.net/webth/cdn/garena/gamertocoder/bmg-characters/",
+							""
+						)
+						.replace(".png", ""),
+					description: `Description for judge #${i}`,
+					imageURI: data.characters[i],
+				};
+				createJudgeSelectBox(data.characters[i], i);
+			}
+		});
+}
+
+function createJudgeSelectBox(data, index) {
+	let box = document.createElement("div");
+	box.innerHTML = `<div class="judge-select-box">
+    <img onClick="setDisplayedJudge(${index})" src="${data}" width="50" height="50">
+  </div>`;
+	document
+		.getElementById("content__judge-information--select")
+		.appendChild(box);
+}
+
+function setDisplayedJudge(index) {
+	document.getElementById("content__judge-information__name").innerText =
+		JUDGE_INFORMATION[index].name;
+	document.getElementById(
+		"judge-image--main"
+	).innerHTML = `<img src=${JUDGE_INFORMATION[index].imageURI}>`;
+}
