45ed84a9524446fd9f2fc97448cbf613599c44fe
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2022-10-29 23:58:46 +0700

    feature: added lore/backstories for judges

--- js/judges.js
+++ js/judges.js
@@ -23,6 +23,14 @@ async function getAndRenderData() {
 						.replace(".png", ""),
 					description: `Description for judge #${i}`,
 					imageURI: data.characters[i],
+					lore: JUDGE_LORE[
+						data.characters[i]
+							.replace(
+								"https://cdngarenanow-a.akamaihd.net/webth/cdn/garena/gamertocoder/bmg-characters/",
+								""
+							)
+							.replace(".png", "")
+					],
 				};
 				createJudgeSelectBox(data.characters[i], i);
 			}
@@ -45,4 +53,7 @@ function setDisplayedJudge(index) {
 	document.getElementById(
 		"judge-image--main"
 	).innerHTML = `<img src=${JUDGE_INFORMATION[index].imageURI}>`;
+	document.getElementById(
+		"content__judge-information__description"
+	).innerText = JUDGE_INFORMATION[index].lore;
 }
